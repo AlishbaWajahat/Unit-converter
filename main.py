b8c0046ea095f40b5ff61fba5dd8753fc4aac260
Alishba Wajahat <159299273+AlishbaWajahat@users.noreply.github.com>
2025-03-26 20:11:24 +0000

    improved error handling

--- main.py
+++ main.py
@@ -1,20 +1,24 @@
 import streamlit as st
 
 def converter(category,value,from_unit,to_unit):
+ try:
+    if value is None or value==0:
+        raise ValueError("Please enter a valid number greater than zero.")
+    if from_unit==to_unit:
+        raise ValueError("The selected units are the same. Please choose different units.")
     if category=="Length":
         if from_unit=="kilometers" and to_unit=="meters":
             return value*1000
         elif from_unit=="meters" and to_unit=="kilometers":
             return value/1000
-        else:
-         raise ValueError("unsupported value")
+
+
     elif category =="Weight":
         if from_unit=="kilograms" and to_unit=="grams":
             return value*1000
         elif from_unit=="grams" and to_unit=="kilograms":
             return value/1000
-        else:
-         raise ValueError("unsupported value")
+
         
     elif category =="Time":
         if from_unit=="hours" and to_unit=="minutes":
@@ -29,14 +33,20 @@ def converter(category,value,from_unit,to_unit):
             return value*3600
         elif from_unit=="seconds" and to_unit=="hours":
             return value/3600
-        else:
-         raise ValueError("unsupported value")
+ except ValueError as e:
+    st.error(f"⚠️ {e}")
+    return None
+ except Exception as e:
+     st.error("⚠️ An unexpected error occurred! Please try again.")
+     return None
+
 
 
 
 def main():
     st.title("Unit converter app") 
-    st.subheader("Welcome to our app 👋")    
+    st.subheader("Welcome to our app 👋")   
+    unit_from, unit_to = None, None 
     category=st.segmented_control("Select a category",["Length","Weight","Time"], selection_mode="single")  
     if category=="Length":
         unit_from=st.selectbox("Select the length's unit you want to convert from",["kilometers","meters"], placeholder="Choose an option",index=None,)
@@ -47,10 +57,15 @@ def main():
     elif category=="Time":
         unit_from=st.selectbox("Select the time's unit you want to convert from",["hours","minutes","seconds"], placeholder="Choose an option",index=None,)
         unit_to=st.selectbox("Select a time's unit you want to convert into",["hours","minutes","seconds"], placeholder="Choose an option",index=None,)
-    value=st.number_input("Enter a value to be converted")
+    value=st.number_input("Enter a value to be converted", min_value=0.0)
     if st.button("Convert"):
-        result=converter(category,value,unit_from,unit_to)
-        st.write(f"The converted value is:" , result)
+       if not unit_from or not unit_to:
+           st.error("⚠️ Please select both 'From' and 'To' units before converting.") 
+       else:   
+         result=converter(category,value,unit_from,unit_to)
+         if result is not None:
+            st.success(f"✅ The converted value is: **{result}**")
+
     
         
-main()            
+main()                       
